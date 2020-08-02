0fedcd5e0fa751176fbb5d2a680db8414f5474fe
hannahjorich <hannah.jorich@gmail.com>
2020-08-02 14:59:25 -0600

    fixed logic, added css

--- script.js
+++ script.js
@@ -10,13 +10,14 @@ var day4 = moment().add(4, 'days').format('M/DD/YYYY');
 var day5 = moment().add(5, 'days').format('M/DD/YYYY');
 
 var apiKey = "f23221b0ff123cd64590007e8f8ef3f3";
+var allCities = []; 
+
 
 $(document).ready(function () {
     $(".btn").on("click", function () {
         var citySearch = $("#city-input").val(); 
         // console.log(citySearch)
         event.preventDefault();
-        var allCities = []; // Array to hold all searched cities
       
         allCities = JSON.parse(localStorage.getItem("allCities")) || []; // Get cities
         allCities.push(citySearch); // pushes new cities entered to array 
@@ -34,20 +35,20 @@ $(document).ready(function () {
             url: `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${apiKey}&units=imperial`,
             dataType: "json",
             success: function (res) {
-                console.log(res)
+                console.log(res)                
                 //create html with Jquery
-                var card = $("<div>").addClass("card col-md-2 ml-4 bg-primary text-white");
+                var card = $("<div>").addClass("card");
                 var cardBody = $("<div>").addClass("card-body");
                 var cardTitle = $("<h3>").addClass("card-title").text(res.name + " " + startDate);
                 var temp = Math.round(res.main.temp);
                 var tempDisplay = $("<p>").addClass("card-text").text("tempture: " + temp + String.fromCharCode(176));
                 var humid = $("<p>").addClass("card-text").text("Humidity: " + res.main.humidity + "%");
                 var wind = $("<p>").addClass("card-text").text("Wind: " + res.wind.speed + "MPH");
-                var weatherIcon = response.weather[0].icon;
-                var image = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + weatherIcon + ".png")
+                // var weatherIcon = response.weather[0].icon;
+                // var image = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + weatherIcon + ".png")
 
 
-                $("#dailyWeather").append(card.append(cardBody.append(cardTitle, tempDisplay, humid, wind, image)))
+                $("#dailyWeather").append(card.append(cardBody.append(cardTitle, tempDisplay, humid, wind)))
 
                 getForecast(res.coord.lat, res.coord.lon);
                 // getUVIndex();
